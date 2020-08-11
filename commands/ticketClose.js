e1d35a24daed581c183ded5fd0a8121db540b730
LuckyCreator <44301423+LuckyCreator@users.noreply.github.com>
2020-08-11 12:50:25 +0200

    Update ticketClose.js

--- commands/ticketClose.js
+++ commands/ticketClose.js
@@ -1,9 +1,20 @@
 const discord = require("discord.js");
 
 module.exports.run = async(bot, message, args) => {
+
+    let logChannel = {
+        logChannel: "715290718608031785"
+    
+    }
     
     if(message.guild.me.hasPermission(`MANAGE_CHANNELS`)) {
         message.channel.delete();
+        let Embed = new discord.MessageEmbed()
+            .setTitle(`Ticket Closed!`)
+            .setDescription(`Ticket Name: ${message.channel.name}\n**Gesloten door: <@${message.author.id}>`)
+            .setColor(`#eb4034`)
+            .setFooter("Chill Place | Official", "https://i.imgur.com/zSWTyii.jpg")
+        message.channel.send(Embed);
     }
 
     if(!message.guild.me.hasPermission(`MANAGE_CHANNELS`)) {
