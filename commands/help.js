24b58f9a8ffbf8616744f3d087377d1ba2677b2d
LuckyCreator <44301423+LuckyCreator@users.noreply.github.com>
2020-08-11 12:42:16 +0200

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -7,7 +7,7 @@ module.exports.run = async(bot, message, args) => {
         .setTitle("**Help**")
         .addField("**__Algemeen__**", "!help - Geeft informatie over commands.\n!idee - Laat een idee achter.\n!avatar - Laat jouw of iemand anders zijn avatar zien.\n!8ball - Stel een vraag en krijg antwoord!\n!sps - Speelt een potje steen papier schaar.")
         .addField("**__Support__**", "!ticket - Maak een ticket aan.\n!close - Sluit een ticket.\n!partner - Zie wat je nodig hebt voor partner.")
-        .addField("**__Staff__**", "t")
+        .addField("**__Staff__**", "!clear - Verwijderd een aantal berichten\n!react - Maak een bericht aan waarmee je een rol krijgt.\n!say - Laat de bot een bericht zeggen.\n!close - Sluit een ticket.")
         .setTimestamp()
         .setFooter("Chill Place | Official", "https://i.imgur.com/zSWTyii.jpg")
     message.channel.send(CreatedTicketEmbed)
